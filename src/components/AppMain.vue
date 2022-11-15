f90ddd0129cc465f15820f35182b745a0e93825b
Andrearusso94 <andrea.r.4845@gmail.com>
2022-11-15 14:45:53 +0100

    push for tiket

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,16 +1,42 @@
 <script>
 import CharaterListVue from './CharaterList.vue';
-
+import SelectBoxVue from './SelectBox.vue';
+import { store } from '../store.js';
+import axios from 'axios'
 export default {
     name: 'AppMain',
-    components: { CharaterListVue },
+    components: { CharaterListVue, SelectBoxVue },
+    data() {
+        return {
+            store
+        }
+    },
+    methods: {
+        selectSeries() {
+            console.log(this.store.selectSeries)
+            const url = `${this.store.API_URL}?category=${selectSeries}`
+            console.log(this.selectSeries);
+
+            axios.get(url)
+                .then(resp => {
+                    this.store.characters = resp.data
+                })
+                .catch(err => {
+                    console.error(err)
+
+                })
+        }
+    }
 
 }
 </script>
 <template>
     <main>
         <div class="category my-5 container">
-            <div class="btn btn-light "> Select category </div>
+            <div class="mb-3">
+
+                <SelectBoxVue @searchData="selectSeries"></SelectBoxVue>
+            </div>
         </div>
 
 
---
+++ src/components/SelectBox.vue
@@ -0,0 +1,22 @@
+<script>
+import { store } from '../store.js'
+export default {
+    name: 'SelectBox',
+    emits: ['searchData'],
+    data() {
+        return {
+            store
+        }
+    }
+
+
+}
+</script>
+
+<template>
+    <select v-model="store.selectSeries" @change="$emit('searchData')" class="form-select form-select-sm" name="" id="">
+        <option selected>breaking bad</option>
+        <option value="">Better Call Soul</option>
+
+    </select>
+</template>
